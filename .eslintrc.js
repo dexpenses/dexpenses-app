e78599846e896dd161843cf7a0205a349cc0ec82
Henrik Drefs <henrik.drefs@outlook.de>
2019-04-06 00:01:02 +0200

    linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true,
   },
   extends: ['plugin:vue/essential', '@vue/airbnb', 'prettier'],
-  //plugins: ['prettier'],
+  // plugins: ['prettier'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
