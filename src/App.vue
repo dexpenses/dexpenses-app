33f8b36c5d7c368483a6add1bba73da13fce6a97
Henrik Drefs <henrik.drefs@outlook.de>
2019-04-12 00:04:58 +0200

    responsive toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <v-app>
-    <v-toolbar app>
+
+    <v-toolbar
+      app
+      clipped-left
+      absolute
+    >
+      <v-toolbar-side-icon
+        class="hidden-sm-and-up"
+        v-if="user"
+        @click.stop="drawer = !drawer"
+      ></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
         <router-link to="/">
           <span>DexmoHQ</span>
@@ -8,24 +18,18 @@
         </router-link>
       </v-toolbar-title>
 
-      <v-toolbar-items v-if="user">
+      <v-toolbar-items
+        v-if="user"
+        class="hidden-sm-and-down"
+      >
         <v-btn
           flat
-          to="/dashboard"
-        >Dashboard
-          <v-icon right>dashboard</v-icon>
-        </v-btn>
-        <v-btn
-          flat
-          to="/receipts"
-        >Receipts
-          <v-icon right>receipt</v-icon>
-        </v-btn>
-        <v-btn
-          flat
-          to="/upload"
-        >Upload
-          <v-icon right>cloud_upload</v-icon>
+          v-for="item in items"
+          :key="item.name"
+          :to="item.link"
+        >
+          {{item.name}}
+          <v-icon right>{{item.icon}}</v-icon>
         </v-btn>
       </v-toolbar-items>
 
@@ -33,7 +37,7 @@
 
       <v-flex
         v-if="user"
-        class="pending-message"
+        class="pending-message hidden-sm-and-down"
       >
         <span v-if="pendingReceiptsCount">
           {{pendingReceiptsCount}} receipts pending.
@@ -48,28 +52,87 @@
         </span>
       </v-flex>
 
-      <v-badge
+      <account-menu
         v-if="user"
-        color="primary"
-        left
-        overlap
-      >
-        <span slot="badge">2</span>
-        <v-btn
-          flat
-          icon
-        >
-          <v-icon medium>notifications</v-icon>
-        </v-btn>
-      </v-badge>
-
-      <account-menu v-if="user" />
+        class="hidden-sm-and-down"
+      />
       <v-btn
         v-else
         color="primary"
         @click="login"
       >LOGIN</v-btn>
     </v-toolbar>
+
+    <v-navigation-drawer
+      v-if="user"
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list class="pa-1">
+        <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img :src="user.photoURL">
+          </v-list-tile-avatar>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{user.displayName}}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+
+      <v-list
+        class="pt-0"
+        dense
+      >
+        <v-divider></v-divider>
+
+        <v-list-tile
+          v-for="item in items"
+          :key="item.name"
+          :to="item.link"
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.name }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-divider></v-divider>
+
+        <v-list-tile
+          ripple
+          to="/settings"
+        >
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              Settings
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile
+          ripple
+          @click="logout"
+        >
+          <v-list-tile-action>
+            <v-icon>logout</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              Logout
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+      </v-list>
+    </v-navigation-drawer>
 
     <v-content>
       <router-view />
@@ -85,6 +148,33 @@ export default {
   name: 'App',
   components: {
     AccountMenu,
+  },
+  data() {
+    return {
+      drawer: false,
+      items: [
+        {
+          name: 'Dashboard',
+          link: '/dashboard',
+          icon: 'dashboard',
+        },
+        {
+          name: 'Receipts',
+          link: '/receipts',
+          icon: 'receipt',
+        },
+        {
+          name: 'Rules',
+          link: '/rules',
+          icon: 'build',
+        },
+        {
+          name: 'Upload',
+          link: '/upload',
+          icon: 'cloud_upload',
+        },
+      ],
+    };
   },
   computed: {
     ...mapState('user', ['user']),
