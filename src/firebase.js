96b8ebb053ac0606956c804673681847008fca4a
Henrik Drefs <henrik.drefs@outlook.de>
2019-05-16 22:56:39 +0200

    include firebase performance

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/storage';
 import 'firebase/firestore';
+import 'firebase/performance';
 
 const config = {
   apiKey: 'AIzaSyCR0QS9ZNiuBIjv70wl3m1PJrkWrDVxrEU',
@@ -13,3 +14,4 @@ const config = {
   appId: '1:45657867106:web:b582be708a26239e',
 };
 firebase.initializeApp(config);
+firebase.performance();
