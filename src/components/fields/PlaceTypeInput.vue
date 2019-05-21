8fe6186c5127ee1d512ed9412fb84d68e3149fea
Henrik Drefs <henrik.drefs@outlook.de>
2019-05-21 19:59:32 +0200

    place type input component

---
+++ src/components/fields/PlaceTypeInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-autocomplete
+    :value="value"
+    @input="$emit('input', $event)"
+    :items="placeTypes"
+    item-text="name"
+    item-value="value"
+    menu-props="auto"
+    label="Place type"
+    hide-details
+    single-line
+  ></v-autocomplete>
+</template>
+<script>
+import { placeTypeMappings } from '@dexpenses/core';
+import { prettifySnakeCase } from '@/util/string';
+
+export default {
+  props: ['value'],
+  data() {
+    return {
+      placeTypes: Object.keys(placeTypeMappings).map(placeType => ({
+        value: placeType,
+        name: prettifySnakeCase(placeType),
+      })),
+    };
+  },
+};
+</script>
--- src/components/rules/PlaceTypeCondition.vue
+++ src/components/rules/PlaceTypeCondition.vue
@@ -1,32 +1,19 @@
 <template>
   <div>
     <span>is</span>
-    <v-autocomplete
+    <PlaceTypeInput
       :value="value"
       @input="$emit('input', $event)"
-      :items="placeTypes"
-      item-text="name"
-      item-value="value"
-      menu-props="auto"
-      label="Place type"
-      hide-details
-      single-line
-    ></v-autocomplete>
+    />
   </div>
 </template>
 <script>
-import { placeTypeMappings } from '@dexpenses/core';
-import { prettifySnakeCase } from '@/util/string';
+import PlaceTypeInput from '@/components/fields/PlaceTypeInput.vue';
 
 export default {
   props: ['value'],
-  data() {
-    return {
-      placeTypes: Object.keys(placeTypeMappings).map(placeType => ({
-        value: placeType,
-        name: prettifySnakeCase(placeType),
-      })),
-    };
+  components: {
+    PlaceTypeInput,
   },
 };
 </script>
