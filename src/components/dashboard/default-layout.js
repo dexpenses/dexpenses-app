041ee7db016840caa42626908d6bbad0649875db
Henrik Drefs <henrik.drefs@outlook.de>
2019-05-29 23:17:07 +0200

    save and load layout from firestore

---
+++ src/components/dashboard/default-layout.js
@@ -0,0 +1,57 @@
+export default [
+  {
+    x: 0,
+    y: 0,
+    w: 3,
+    h: 2,
+    i: '0',
+    component: 'Kpi',
+    props: { title: 'Average monthly total', func: 'aggregateAverageTotal' },
+  },
+  {
+    x: 3,
+    y: 0,
+    w: 3,
+    h: 2,
+    i: '1',
+    component: 'Kpi',
+    props: {
+      title: 'Current month',
+      func: 'aggregateTotal',
+      data: {
+        start: {
+          $now: {
+            set: { day: 1, hour: 0, minute: 0, second: 0, millisecond: 0 },
+          },
+        },
+      },
+    },
+  },
+  { x: 6, y: 0, w: 3, h: 2, i: '2', component: 'TagCloud' },
+  {
+    x: 9,
+    y: 0,
+    w: 3,
+    h: 2,
+    i: '3',
+    component: 'DoughnutAggregation',
+    props: { func: 'aggregateByPaymentMethod' },
+  },
+  {
+    x: 0,
+    y: 3,
+    w: 6,
+    h: 3,
+    i: '6',
+    component: 'ExpensesOverTime',
+  },
+  {
+    x: 6,
+    y: 3,
+    w: 6,
+    h: 3,
+    i: '7',
+    component: 'Map',
+    props: { center: { lat: 52.427547, lng: 10.78042 } },
+  },
+];
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,92 +1,52 @@
 <template>
-  <div>
-    <GridLayout
-      :layout.sync="layout"
-      :use-css-transforms="true"
-      :is-draggable="editing"
-      :is-resizable="editing"
-      :responsive="true"
-      :col-num="12"
-      @layout-updated="layoutUpdated"
+  <GridLayout
+    v-if="layout"
+    :layout.sync="layout"
+    :use-css-transforms="true"
+    :is-draggable="editing"
+    :is-resizable="editing"
+    :responsive="true"
+    :col-num="12"
+    @layout-updated="layoutUpdated"
+  >
+    <GridItem
+      class="dashboard-item"
+      v-for="item in layout"
+      :key="item.i"
+      :x="item.x"
+      :y="item.y"
+      :w="item.w"
+      :h="item.h"
+      :i="item.i"
     >
-      <GridItem
-        class="dashboard-item"
-        v-for="item in layout"
-        :key="item.i"
-        :x="item.x"
-        :y="item.y"
-        :w="item.w"
-        :h="item.h"
-        :i="item.i"
-      >
-        <component
-          :is="item.component"
-          v-bind="item.props"
-        />
-      </GridItem>
-    </GridLayout>
+      <component
+        :is="item.component"
+        v-bind="item.props"
+      />
+    </GridItem>
+  </GridLayout>
+  <div
+    class="loading-container"
+    v-else
+  >
+    <span>Loading dashboard...</span>
+    <v-progress-circular
+      indeterminate
+      size="64"
+    ></v-progress-circular>
   </div>
 </template>
 <script>
+import firebase from 'firebase/app';
+import { mapActions } from 'vuex';
 import { GridLayout, GridItem } from 'vue-grid-layout';
 import ExpensesOverTime from '@/components/dashboard/ExpensesOverTime.vue';
 import TagCloud from '@/components/dashboard/TagCloud.vue';
 import DoughnutAggregation from '@/components/dashboard/DoughnutAggregation.vue';
 import Map from '@/components/dashboard/Map.vue';
 import Kpi from '@/components/dashboard/Kpi.vue';
+import defaultLayout from '@/components/dashboard/default-layout';
 import { parseProps } from '@/util/relative-dates';
-
-const testLayout = [
-  {
-    x: 0,
-    y: 0,
-    w: 3,
-    h: 2,
-    i: '0',
-    component: 'Kpi',
-    props: { title: 'Average monthly total', func: 'aggregateAverageTotal' },
-  },
-  {
-    x: 3,
-    y: 0,
-    w: 3,
-    h: 2,
-    i: '1',
-    component: 'Kpi',
-    props: {
-      title: 'Current month',
-      func: 'aggregateTotal',
-      data: { start: '$firstOfMonth' },
-    },
-  },
-  { x: 6, y: 0, w: 3, h: 2, i: '2', component: 'TagCloud' },
-  {
-    x: 9,
-    y: 0,
-    w: 3,
-    h: 2,
-    i: '3',
-    component: 'DoughnutAggregation',
-    props: { func: 'aggregateByPaymentMethod' },
-  },
-  {
-    x: 0,
-    y: 3,
-    w: 6,
-    h: 3,
-    i: '6',
-    component: 'ExpensesOverTime',
-  },
-  {
-    x: 6,
-    y: 3,
-    w: 6,
-    h: 3,
-    i: '7',
-    component: 'Map',
-    props: { center: { lat: 52.427547, lng: 10.78042 } },
-  },
-];
 
 export default {
   name: 'Dashboard',
@@ -101,22 +61,48 @@ export default {
   },
   data() {
     return {
-      layout: testLayout.map(item => ({
-        ...item,
-        props: parseProps(item.props),
-      })),
+      layout: null,
       editing: false,
     };
   },
   methods: {
+    ...mapActions(['user/checkLoggedIn']),
     layoutUpdated(e) {
       console.log(e);
     },
+  },
+  async mounted() {
+    const user = await this['user/checkLoggedIn']();
+    const ref = await firebase
+      .firestore()
+      .doc(`dashboardsByUser/${user.uid}/dashboards/default`)
+      .get();
+    let layout;
+    if (!ref.exists) {
+      layout = defaultLayout;
+    } else {
+      ({ layout } = ref.data());
+    }
+    this.layout = layout.map(item => ({
+      ...item,
+      props: parseProps(item.props),
+    }));
   },
 };
 </script>
 <style scoped>
 .dashboard-item > div {
   height: 100%;
+}
+.loading-container {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+.loading-container span {
+  margin: 1em;
+  font-size: 1.5em;
 }
 </style>
