ab36c83b47b305ec43bbd1a50bd2e0ae342d2386
Henrik Drefs <henrik.drefs@outlook.de>
2019-06-01 13:33:23 +0200

    fix polyfill for node

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     '^.+\\.jsx?$': 'babel-jest',
   },
+  setupFiles: ['<rootDir>/tests/setup.js'],
   transformIgnorePatterns: ['/node_modules/'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
