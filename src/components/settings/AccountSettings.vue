ce59b12c61ae62a32832c659a73ae38fcec8d9da
Henrik Drefs <henrik.drefs@outlook.de>
2019-08-08 20:59:36 +0200

    fix gmap style correctly follows vuetify theme

--- src/components/settings/AccountSettings.vue
+++ src/components/settings/AccountSettings.vue
@@ -92,7 +92,7 @@
         :center="userData.homeLocation"
         :zoom="15"
         map-type-id="terrain"
-        :options="$vuetify.theme.dark ? darkMapOptions : {}"
+        :options="gmapOptions"
         style="width: 400px; height: 400px"
       >
         <GmapMarker
@@ -112,6 +112,7 @@ import firebase from 'firebase/app';
 import GmapAutocomplete from '@/components/GmapAutocomplete.vue';
 import PhoneNumberInput from '@/components/fields/PhoneNumberInput.vue';
 import languages from '@/i18n/langs';
+import gmapDarkTheme from '@/components/gmap-dark-theme';
 
 const { mapState } = createNamespacedHelpers('user');
 
@@ -130,95 +131,16 @@ export default {
       phoneNumberError: null,
       languages,
       showBanner: true,
-      darkMapOptions: {
-        styles: [
-          { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
-          {
-            elementType: 'labels.text.stroke',
-            stylers: [{ color: '#242f3e' }],
-          },
-          { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
-          {
-            featureType: 'administrative.locality',
-            elementType: 'labels.text.fill',
-            stylers: [{ color: '#d59563' }],
-          },
-          {
-            featureType: 'poi',
-            elementType: 'labels.text.fill',
-            stylers: [{ color: '#d59563' }],
-          },
-          {
-            featureType: 'poi.park',
-            elementType: 'geometry',
-            stylers: [{ color: '#263c3f' }],
-          },
-          {
-            featureType: 'poi.park',
-            elementType: 'labels.text.fill',
-            stylers: [{ color: '#6b9a76' }],
-          },
-          {
-            featureType: 'road',
-            elementType: 'geometry',
-            stylers: [{ color: '#38414e' }],
-          },
-          {
-            featureType: 'road',
-            elementType: 'geometry.stroke',
-            stylers: [{ color: '#212a37' }],
-          },
-          {
-            featureType: 'road',
-            elementType: 'labels.text.fill',
-            stylers: [{ color: '#9ca5b3' }],
-          },
-          {
-            featureType: 'road.highway',
-            elementType: 'geometry',
-            stylers: [{ color: '#746855' }],
-          },
-          {
-            featureType: 'road.highway',
-            elementType: 'geometry.stroke',
-            stylers: [{ color: '#1f2835' }],
-          },
-          {
-            featureType: 'road.highway',
-            elementType: 'labels.text.fill',
-            stylers: [{ color: '#f3d19c' }],
-          },
-          {
-            featureType: 'transit',
-            elementType: 'geometry',
-            stylers: [{ color: '#2f3948' }],
-          },
-          {
-            featureType: 'transit.station',
-            elementType: 'labels.text.fill',
-            stylers: [{ color: '#d59563' }],
-          },
-          {
-            featureType: 'water',
-            elementType: 'geometry',
-            stylers: [{ color: '#17263c' }],
-          },
-          {
-            featureType: 'water',
-            elementType: 'labels.text.fill',
-            stylers: [{ color: '#515c6d' }],
-          },
-          {
-            featureType: 'water',
-            elementType: 'labels.text.stroke',
-            stylers: [{ color: '#17263c' }],
-          },
-        ],
-      },
     };
   },
   computed: {
     ...mapState(['userData', 'user']),
+    gmapOptions() {
+      if (this.$vuetify.theme.isDark) {
+        return { styles: gmapDarkTheme };
+      }
+      return { styles: [] };
+    },
   },
   watch: {
     userData(v) {
