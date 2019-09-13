8e140f34121ad35d72e3744d383fff71915e7048
Henrik Drefs <henrik.drefs@outlook.de>
2019-09-13 21:47:45 +0200

    added router auth guards, closes #258

--- src/router.js
+++ src/router.js
@@ -4,6 +4,15 @@ import store from './store';
 
 Vue.use(Router);
 
+async function authGuard(to, from, next) {
+  const user = await store.dispatch('user/checkLoggedIn');
+  if (!user) {
+    next({ name: 'home' });
+  } else {
+    next();
+  }
+}
+
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
@@ -25,41 +34,49 @@ export default new Router({
       path: '/dashboard',
       name: 'dashboard',
       component: () => import('./views/Dashboard.vue'),
+      beforeEnter: authGuard,
     },
     {
       path: '/recurring',
       name: 'recurring',
       component: () => import('./views/Recurring.vue'),
+      beforeEnter: authGuard,
     },
     {
       path: '/receipts',
       name: 'receipts',
       component: () => import('./views/Receipts.vue'),
+      beforeEnter: authGuard,
     },
     {
       path: '/upload',
       name: 'uploadReceipts',
       component: () => import('./views/UploadReceipts.vue'),
+      beforeEnter: authGuard,
     },
     {
       path: '/rules',
       name: 'rules',
       component: () => import('./views/Rules.vue'),
+      beforeEnter: authGuard,
     },
     {
       path: '/settings',
       name: 'settings',
       component: () => import('./views/Settings.vue'),
+      beforeEnter: authGuard,
     },
     {
       path: '/search',
       name: 'search',
       component: () => import('./views/Search.vue'),
+      beforeEnter: authGuard,
     },
     {
       path: '/admin',
       name: 'admin',
       component: () => import('./views/admin/Admin.vue'),
+      beforeEnter: authGuard,
       children: [
         {
           path: 'test-data/upload',
